c11d98d25e65cee558029575fa5ba31d2c0a6f97
Simon Vergauwen <nomisRev@users.noreply.github.com>
2025-07-14 20:52:15 +0200

    Migrate project to Kotlin Multiplatform, introduce build-logic composite build, and centralize test resources for common compatibility.

--- typed-api/build.gradle.kts
+++ typed-api/build.gradle.kts
@@ -1,16 +1,22 @@
 plugins {
-    kotlin("jvm")
+    id("kotlin-multiplatform-common")
     kotlin("plugin.serialization")
     kotlin("plugin.power-assert")
     id("org.jetbrains.kotlinx.kover")
 }
 
-dependencies {
-    api(libs.kotlinx.serialization)
-    api(libs.kotlinx.coroutines)
-    
-    testImplementation(kotlin("test"))
-    testImplementation("org.junit.platform:junit-platform-launcher:1.10.1")
-    testImplementation("org.junit.platform:junit-platform-engine:1.10.1")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.1")
+kotlin {
+    sourceSets {
+        commonMain {
+            dependencies {
+                api(libs.kotlinx.serialization)
+                api(libs.kotlinx.coroutines)
+            }
+        }
+        commonTest {
+            dependencies {
+                implementation(libs.kotlin.test)
+            }
+        }
+    }
 }
